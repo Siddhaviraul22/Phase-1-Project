ddbff77e9d21314141ca0a2d19311bb3ecc2984d
Siddhaviraul22 <132128142+Siddhaviraul22@users.noreply.github.com>
2024-01-05 00:44:50 +0530

    Update Camera.java

--- Phase-1 Assessment Project/src/Camerarentalaapp/Camera.java
+++ Phase-1 Assessment Project/src/Camerarentalaapp/Camera.java
@@ -1,39 +1,43 @@
 package Camerarentalaapp;
 
 public class Camera {
-    private static int nextCameraId = 1;
+	    private static int nextCameraId = 1;
 
-    private int cameraId;
-    private String brand;
-    private String model;
-    private double perDayRent;
-    private String status;
+	    private int cameraId;
+	    private String brand;
+	    private String model;
+	    private double perDayRent;
+	    private String status;
 
-    public Camera(String brand, String model, double perDayRent, String status) {
-        this.cameraId = nextCameraId++;
-        this.brand = brand;
-        this.model = model;
-        this.perDayRent = perDayRent;
-        this.status = status;
-    }
+	    public Camera(String brand, String model, double perDayRent, String status) {
+	        this.cameraId = nextCameraId++;
+	        this.brand = brand;
+	        this.model = model;
+	        this.perDayRent = perDayRent;
+	        this.status = status;
+	    }
 
-    public int getCameraId() {
-        return cameraId;
-    }
+	    public int getCameraId() {
+	        return cameraId;
+	    }
 
-    public String getBrand() {
-        return brand;
-    }
+	    public String getBrand() {
+	        return brand;
+	    }
 
-    public String getModel() {
-        return model;
-    }
+	    public String getModel() {
+	        return model;
+	    }
 
-    public double getPerDayRent() {
-        return perDayRent;
-    }
+	    public double getPerDayRent() {
+	        return perDayRent;
+	    }
 
-    public String getStatus() {
-        return status;
-    }
-}
+	    public String getStatus() {
+	        return status;
+	    }
+
+	    public void setStatus(String status) {
+	        this.status = status;
+	    }
+	}
