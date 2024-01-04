2e9b57bc651c44dc69b9e2893587427465132994
Siddhaviraul22 <132128142+Siddhaviraul22@users.noreply.github.com>
2024-01-05 01:22:47 +0530

    Update User.java

--- Phase-1 Assessment Project/src/Camerarentalaapp/User.java
+++ Phase-1 Assessment Project/src/Camerarentalaapp/User.java
@@ -14,4 +14,16 @@ public class User {
     public void setWalletBalance(double walletBalance) {
         this.walletBalance = walletBalance;
     }
+
+    public void depositToWallet(double amount) {
+        try {
+            if (amount <= 0) {
+                throw new IllegalArgumentException("Invalid deposit amount. Amount must be greater than 0.");
+            }
+            setWalletBalance(getWalletBalance() + amount);
+            System.out.println("Deposit successful. New wallet balance: $" + getWalletBalance());
+        } catch (IllegalArgumentException e) {
+            System.out.println("Deposit failed. " + e.getMessage());
+        }
+    }
 }
