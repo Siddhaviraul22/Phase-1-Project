8ad29977f6e4d757df2f6bb6b53193b4cf32586a
siddhaviraul22 <raulsiddhavi678@gmial.com>
2024-01-04 19:20:05 +0530

    Phase 1 project (Created Camera Class)

---
+++ Phase-1 Assessment Project/src/Camerarentalaapp/Camera.java
@@ -0,0 +1,39 @@
+package Camerarentalaapp;
+
+public class Camera {
+    private static int nextCameraId = 1;
+
+    private int cameraId;
+    private String brand;
+    private String model;
+    private double perDayRent;
+    private String status;
+
+    public Camera(String brand, String model, double perDayRent, String status) {
+        this.cameraId = nextCameraId++;
+        this.brand = brand;
+        this.model = model;
+        this.perDayRent = perDayRent;
+        this.status = status;
+    }
+
+    public int getCameraId() {
+        return cameraId;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public double getPerDayRent() {
+        return perDayRent;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+}
